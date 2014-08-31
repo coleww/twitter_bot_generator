88aec89de801bd0086539618b8c741f7cf29795a
Cole Willsea <coleww@gmail.com>
2014-08-30 18:33:57 -0700

    one test passing!

--- test/scaffold_generator_test.rb
+++ test/scaffold_generator_test.rb
@@ -1,14 +1,10 @@
-require 'minitest/unit'
+require 'minitest/autorun'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'fakefs'
 require_relative "../lib/twitter_bot_generator.rb"
 
 class TestTwitterBotGenerator <  MiniTest::Test
-  def setup
-  end
-
-  def teardown
-  end
 
   def test_it_generates_something
     assert_respond_to TwitterBotGenerator, :generate
