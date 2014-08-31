cb3555537378ef8b3c22392e77826de37644a8dc
Cole Willsea <coleww@gmail.com>
2014-08-30 18:49:30 -0700

    tests for directory creation

--- test/scaffold_generator_test.rb
+++ test/scaffold_generator_test.rb
@@ -14,4 +14,56 @@ class TestTwitterBotGenerator <  MiniTest::Test
     assert_equal true, (Dir.exists? './such_test_bot')
   end
 
+  def test_it_creates_a_lib_directory
+    assert_equal true, (Dir.exists? './such_test_bot/lib')
+  end
+
+  def test_it_creates_a_src_directory
+    assert_equal true, (Dir.exists? './such_test_bot/src')
+  end
+
+  def test_it_creates_src_helper_dir
+    assert_equal true, (Dir.exists? './such_test_bot/src/such_test_bot')
+  end
+
+  def test_it_creates_a_test_directory
+    assert_equal true, (Dir.exists? './such_test_bot/test')
+  end
+
+  # def test_it_creates_a_gitignore_with_defaults
+  #   assert_equal true, (Dir.exists? './such_test_bot')
+  # end
+
+  # def test_it_creates_a_gemfile_with_defaults
+  #   assert_equal true, (Dir.exists? './such_test_bot')
+  # end
+
+  # def test_it_creates_a_readme_with_defaults
+  #   assert_equal true, (Dir.exists? './such_test_bot')
+  # end
+
+  # def test_it_creates_a_procfile_with_defaults
+  #   assert_equal true, (Dir.exists? './such_test_bot')
+  # end
+
+  # def test_it_creates_a_spec_runner_with_defaults
+  #   assert_equal true, (Dir.exists? './such_test_bot/src')
+  # end
+
+  # def test_it_creates_a_test_runner_with_defaults
+  #   assert_equal true, (Dir.exists? './such_test_bot/src')
+  # end
+
+  # def test_it_creates_a_test_helper_with_defaults
+  #   assert_equal true, (Dir.exists? './such_test_bot/src')
+  # end
+
+  # def test_it_creates_a_smokescreen_test
+  #   assert_equal true, (Dir.exists? './such_test_bot/src')
+  # end
+
+  # def test_it_creates_class_file
+  #   assert_equal true, (Dir.exists? './such_test_bot/src')
+  # end
+
 end
