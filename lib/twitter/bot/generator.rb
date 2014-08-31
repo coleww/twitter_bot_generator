1fc38e4de2cb00dd9559164214a88c4f87aa0fd4
Cole Willsea <coleww@gmail.com>
2014-08-30 17:43:46 -0700

    initial

---
+++ lib/twitter/bot/generator.rb
@@ -0,0 +1,9 @@
+require "twitter/bot/generator/version"
+
+module Twitter
+  module Bot
+    module Generator
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/twitter/bot/generator/version.rb
@@ -0,0 +1,7 @@
+module Twitter
+  module Bot
+    module Generator
+      VERSION = "0.0.1"
+    end
+  end
+end
