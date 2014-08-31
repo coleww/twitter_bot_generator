8a483a9df858e84eb38ea26d15c3eda581c82bdc
Cole Willsea <coleww@gmail.com>
2014-08-30 19:14:51 -0700

    testing for all the files AFAIK

--- lib/twitter_bot_generator.rb
+++ lib/twitter_bot_generator.rb
@@ -7,15 +7,35 @@ class TwitterBotGenerator
   class << self
 
     def generate bot_name
+      @@bot_name = bot_name
       Dir.mkdir bot_name
       folders.each do |folder|
         Dir.mkdir bot_name + '/' + folder
+      end
+      files.keys.each do |file|
+        File.open bot_name + '/' + file, 'w' do |f|
+          f.write files[file]
+        end
       end
     end
 
   private
     def folders
       %w(lib src test)
+    end
+
+    def files
+      {
+        '.gitignore' => '',
+        'Gemfile' => '',
+        'README.md' => '',
+        'Procfile' => '',
+        'spec.rb' => '',
+        'test.rb' => '',
+        'test/' + @@bot_name + '_test.rb' => '',
+        'src/'  + @@bot_name + '.rb' => '',
+        'lib/.gitkeep' => ''
+      }
     end
 
   end
--- test/scaffold_generator_test.rb
+++ test/scaffold_generator_test.rb
@@ -26,40 +26,40 @@ class TestTwitterBotGenerator <  MiniTest::Test
     assert_equal true, (Dir.exists? './such_test_bot/test')
   end
 
-  # def test_it_creates_a_gitignore_with_defaults
-  #   assert_equal true, (Dir.exists? './such_test_bot')
-  # end
+  def test_it_creates_a_gitignore_with_defaults
+    assert_equal true, (File.exists? './such_test_bot/.gitignore')
+  end
 
-  # def test_it_creates_a_gemfile_with_defaults
-  #   assert_equal true, (Dir.exists? './such_test_bot')
-  # end
+  def test_it_creates_a_gemfile_with_defaults
+    assert_equal true, (File.exists? './such_test_bot/Gemfile')
+  end
 
-  # def test_it_creates_a_readme_with_defaults
-  #   assert_equal true, (Dir.exists? './such_test_bot')
-  # end
+  def test_it_creates_a_readme_with_defaults
+    assert_equal true, (File.exists? './such_test_bot/README.md')
+  end
 
-  # def test_it_creates_a_procfile_with_defaults
-  #   assert_equal true, (Dir.exists? './such_test_bot')
-  # end
+  def test_it_creates_a_procfile_with_defaults
+    assert_equal true, (File.exists? './such_test_bot/Procfile')
+  end
 
-  # def test_it_creates_a_spec_runner_with_defaults
-  #   assert_equal true, (Dir.exists? './such_test_bot/src')
-  # end
+  def test_it_creates_a_spec_runner_with_defaults
+    assert_equal true, (File.exists? './such_test_bot/spec.rb')
+  end
 
-  # def test_it_creates_a_test_runner_with_defaults
-  #   assert_equal true, (Dir.exists? './such_test_bot/src')
-  # end
+  def test_it_creates_a_test_runner_with_defaults
+    assert_equal true, (File.exists? './such_test_bot/test.rb')
+  end
 
-  # def test_it_creates_a_test_helper_with_defaults
-  #   assert_equal true, (Dir.exists? './such_test_bot/src')
-  # end
+  def test_it_creates_a_smokescreen_test
+    assert_equal true, (File.exists? './such_test_bot/test/such_test_bot_test.rb')
+  end
 
-  # def test_it_creates_a_smokescreen_test
-  #   assert_equal true, (Dir.exists? './such_test_bot/src')
-  # end
+  def test_it_creates_boilerplate_class_file
+    assert_equal true, (File.exists? './such_test_bot/src/such_test_bot.rb')
+  end
 
-  # def test_it_creates_class_file
-  #   assert_equal true, (Dir.exists? './such_test_bot/src')
-  # end
+  def test_it_creates_bin_slash_gitkeep
+    assert_equal true, (File.exists? './such_test_bot/lib/.gitkeep')
+  end
 
 end
