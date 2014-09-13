b743563cb2099b3500bb5b19be1185e562c7e69a
Cole Willsea <coleww@gmail.com>
2014-09-13 15:54:43 -0700

    committing an examplebot...cuz...idk

---
+++ such_test_bot/src/such_test_bot.rb
@@ -0,0 +1,25 @@
+class SuchTestBot
+
+  class << self
+
+    def generate
+      greetings.sample
+    end
+
+  protected
+
+    def greetings
+      @greetings ||= load_txt_file 'greetings.txt'
+    end
+
+  private
+
+    def load_txt_file file_name
+      home_sweet_home = File.expand_path File.dirname __FILE__
+      ultimate_pathway = File.join home_sweet_home, '..', 'lib', file_name
+      (File.readlines ultimate_pathway).map &:strip
+    end
+
+  end
+
+end
